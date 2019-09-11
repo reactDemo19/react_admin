d383e275e70296cc10565791a0be996e54e990b7
liule1001 <1762219852@qq.com>
2019-09-11 19:13:28 +0800

    '主城搭建完毕'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'
+import axios from 'untils/axios.js'
 import * as serviceWorker from './serviceWorker';
-
+React.Component.prototype.$axios=axios
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
